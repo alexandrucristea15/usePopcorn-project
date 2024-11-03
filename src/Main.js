b67dbe4f0909cf9b0247cd0379d7da58bb7fc223
Cristea Alexandru <alex.p.cristea@gmail.com>
2024-11-03 18:28:15 +0200

    Initial commit , halfway point in the lesson

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+const Main = ({ movies, tempWatchedData }) => {
+  return (
+    <main className="main">
+      <ListBox movies={movies} />
+      <WatchedBox tempWatchedData={tempWatchedData} />
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Search from "./Search";
+import Logo from "./Logo";
+import NumResults from "./NumResults";
+
+const NavBar = ({ movies }) => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults movies={movies} />
+    </nav>
+  );
+};
+
+export default NavBar;
