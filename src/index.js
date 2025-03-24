2da78cc834e8e9a348089030de4bde5e8ca58f6e
Cristea Alexandru <alex.p.cristea@gmail.com>
2025-03-24 22:10:22 +0200

    - Created StarRating Component - Made the component reausable with a bunch of props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./Components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Great"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={82}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
